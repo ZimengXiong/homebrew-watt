c65d5018a4758252dc69ad3ae85b9b25af3300fa
Zimeng Xiong <zxzimeng@gmail.com>
2026-01-10 12:22:38 -0800

    Update to v1.0.8

--- Casks/watt.rb
+++ Casks/watt.rb
@@ -2,8 +2,8 @@
 # frozen_string_literal: true
 
 cask "watt" do
-  version "1.0.7"
-  sha256 "aaa3c126a09f66bdfba9811872d15d0e8718959938dc6eb8bcad751ce839692c"
+  version "1.0.8"
+  sha256 "524461b7719926cb59fe40b3112811423198dc13655aa13234a60a8140a8f56b"
 
   url "https://github.com/zimengxiong/watt/releases/download/v#{version}/Watt.app.zip"
   name "Watt"
