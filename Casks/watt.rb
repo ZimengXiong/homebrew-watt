f0b87aa3250e360ff67e37f1bebc0ca562c50687
Zimeng Xiong <zxzimeng@gmail.com>
2026-07-07 15:09:05 -0700

    Re-sign v1.5.4 with development cert

--- Casks/watt.rb
+++ Casks/watt.rb
@@ -3,7 +3,7 @@
 
 cask "watt" do
   version "1.5.4"
-  sha256 "779085de7f913ddb5b8a68d3ba0a2e495734527833f80563b70ec585e12da0fc"
+  sha256 "12e7cdd8f723dec802185e5d6c2b450598ca22eadafc2a8ff3eb225d467a278f"
 
   url "https://github.com/zimengxiong/watt/releases/download/v#{version}/Watt.app.zip"
   name "Watt"
